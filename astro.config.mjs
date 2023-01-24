c1c32b39f3deef230e7330c40947259364d2c998
Hans5958 <hans5958@outlook.com>
2023-01-24 13:46:46 +0700

    Progress as of 24/01/2023

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
@@ -8,5 +7,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [sitemap(), tailwind()]
 });
--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Hans5958';
+export const SITE_DESCRIPTION = 'The website of Hans5958.'
+export const SITE_AUTHOR = 'Hans5958'
+export const SITE_REPOSITORY = 'https://github.com/Hans5958/hans5958.github.io'
+export const SITE_TWITTER = 'Hans5958'
+export const COPYRIGHT_YEAR = 2023
