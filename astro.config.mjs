e5f57d933a852b8c1103847f1a94793a1b51b493
Hans5958 <hans5958@outlook.com>
2023-05-01 23:39:49 +0700

    Update dependencies, use Assets feature

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,21 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
-import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://hans5958.github.io',
   integrations: [sitemap(), tailwind(), 
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp'
-    }), 
     import.meta.env.MODE === "production" && compress({
-    img: false
-  })]
+      img: false
+    })
+  ],
+  experimental: {
+    assets: true
+  },
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp' 
+    },
+  },
 });
