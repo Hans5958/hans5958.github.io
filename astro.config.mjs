dfa06105d484f48d8f7fb1d03ee82c997e5d7179
Hans5958 <hans5958@outlook.com>
2024-01-27 19:16:34 +0700

    Update dependencies and config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,23 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
+import icon from "astro-icon";
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://hans5958.github.io/',
-  integrations: [sitemap(), tailwind(), 
-    import.meta.env.MODE === "production" && compress({
-      img: false
-    })
-  ],
-  image: {
-    service: {
-      entrypoint: 'astro/assets/services/sharp' 
-    },
-  },
-  compressHTML: false
+	site: 'https://hans5958.github.io/',
+	integrations: [
+		sitemap(), tailwind(), icon(), vue(),
+		import.meta.env.MODE === "production" && compress({
+			img: false
+		})
+	],
+	image: {
+		remotePatterns: [{ protocol: "https" }],
+		service: {
+			entrypoint: 'astro/assets/services/sharp'
+		}
+	},
+	compressHTML: false
 });
