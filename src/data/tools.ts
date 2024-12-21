48559bd471ce49e570684dc071de021eeaa61a49
Hans5958 <hans5958@outlook.com>
2024-12-21 10:25:21 +0700

    Add more tools, adjust order on home page

---
+++ src/data/tools.ts
@@ -0,0 +1,231 @@
+export default [
+	{
+		"name": "Web",
+		"tools": [
+			{
+				"icon": "simple-icons:html5",
+				"name": "HTML 5"
+			},
+			{
+				"icon": "simple-icons:javascript",
+				"name": "JavaScript"
+			},
+			{
+				"icon": "simple-icons:css3",
+				"name": "CSS"
+			},
+			{
+				"icon": "simple-icons:sass",
+				"name": "SCSS"
+			},
+			{
+				"icon": "simple-icons:bootstrap",
+				"name": "Bootstrap"
+			},
+			{
+				"icon": "simple-icons:tailwindcss",
+				"name": "Tailwind CSS"
+			},
+			{
+				"icon": "simple-icons:jekyll",
+				"name": "Jekyll"
+			},
+			{
+				"icon": "simple-icons:hugo",
+				"name": "Hugo"
+			},
+			{
+				"icon": "simple-icons:vuedotjs",
+				"name": "Vue"
+			},
+			{
+				"icon": "simple-icons:astro",
+				"name": "Astro"
+			},
+			{
+				"icon": "simple-icons:nuxtdotjs",
+				"name": "Nuxt.js"
+			},
+			{
+				"icon": "simple-icons:netlify",
+				"name": "Netlify"
+			},
+			{
+				"icon": "simple-icons:heroku",
+				"name": "Heroku"
+			},
+			{
+				"icon": "simple-icons:railway",
+				"name": "Railway"
+			},
+			{
+				"icon": "simple-icons:cloudflare",
+				"name": "Cloudflare"
+			},
+			{
+				"icon": "simple-icons:react",
+				"name": "React"
+			},
+			{
+				"icon": "simple-icons:nextdotjs",
+				"name": "Next.js"
+			},
+			{
+				"icon": "simple-icons:typescript",
+				"name": "Typescript"
+			},
+			{
+				"icon": "simple-icons:markdown",
+				"name": "Markdown"
+			},
+			{
+				"icon": "simple-icons:visualstudiocode",
+				"name": "VS Code"
+			},
+			{
+				"icon": "simple-icons:webstorm",
+				"name": "WebStorm"
+			},
+		]
+	},
+	{
+		"name": "Python",
+		"tools": [
+			{
+				"icon": "simple-icons:python",
+				"name": "Python"
+			},
+			{
+				"icon": "simple-icons:flask",
+				"name": "Flask"
+			},
+			{
+				"icon": "simple-icons:selenium",
+				"name": "Selenium"
+			},
+			{
+				"icon": "simple-icons:visualstudiocode",
+				"name": "VS Code"
+			},
+			{
+				"icon": "simple-icons:pycharm",
+				"name": "PyCharm"
+			},
+		]
+	},
+	{
+		"name": "OOP and Mobile",
+		"tools": [
+			{
+				"icon": "devicon-plain:java",
+				"name": "Java"
+			},
+			{
+				"icon": "simple-icons:flutter",
+				"name": "Flutter"
+			},
+			{
+				"icon": "simple-icons:dart",
+				"name": "Dart"
+			},
+			{
+				"icon": "simple-icons:kotlin",
+				"name": "Kotlin"
+			},
+			{
+				"icon": "simple-icons:android",
+				"name": "Android"
+			},
+			{
+				"icon": "simple-icons:intellijidea",
+				"name": "IntelliJ IDEA"
+			},
+			{
+				"icon": "simple-icons:androidstudio",
+				"name": "Android Studio"
+			},		
+		]
+	},
+	{
+		"name": "Operations and Cloud",
+		"tools": [
+			{
+				"icon": "simple-icons:github",
+				"name": "GitHub"
+			},
+			{
+				"icon": "simple-icons:gitlab",
+				"name": "GitLab"
+			},
+			{
+				"icon": "simple-icons:githubactions",
+				"name": "GitHub Actions"
+			},
+			{
+				"icon": "simple-icons:travisci",
+				"name": "Travis CI"
+			},
+			{
+				"icon": "simple-icons:googlecloud",
+				"name": "Google Cloud"
+			},
+			{
+				"icon": "simple-icons:amazonwebservices",
+				"name": "AWS"
+			},		
+			{
+				"icon": "simple-icons:docker",
+				"name": "Docker"
+			},				
+		]
+	},
+	{
+		"name": "Other",
+		"tools": [
+			{
+				"icon": "simple-icons:gnubash",
+				"name": "Bash"
+			},
+			{
+				"icon": "simple-icons:dart",
+				"name": "Dart"
+			},
+			{
+				"icon": "simple-icons:c",
+				"name": "C"
+			},
+			{
+				"icon": "simple-icons:lua",
+				"name": "Lua"
+			},
+			{
+				"icon": "simple-icons:mongodb",
+				"name": "MongoDB"
+			},
+			{
+				"icon": "simple-icons:postgresql",
+				"name": "PostgreSQL"
+			},
+			{
+				"icon": "simple-icons:graphql",
+				"name": "GraphQL"
+			},
+			{
+				"icon": "simple-icons:git",
+				"name": "Git"
+			},
+			{
+				"icon": "simple-icons:jsonwebtokens",
+				"name": "JWT"
+			},
+			{
+				"icon": "simple-icons:visualstudio",
+				"name": "Visual Studio"
+			},
+			{
+				"icon": "simple-icons:alpinelinux",
+				"name": "Alpine Linux"
+			},
+		]
+	}
+]
