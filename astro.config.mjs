a9b7baf97ac1207749d6827d547a67da29bf1afa
Hans5958 <hans5958@outlook.com>
2025-05-08 23:12:38 +0700

    Update dependencies, fix build errors
    Also includes migration to Tailwind CSS v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,28 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
-import compress from "astro-compress";
-import icon from "astro-icon";
-import vue from "@astrojs/vue";
+import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
+import tailwindcss from '@tailwindcss/vite'
+import vue from "@astrojs/vue"
+import compress from "astro-compress"
+import icon from "astro-icon"
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://hans5958.github.io/',
+
 	integrations: [
-		sitemap(), tailwind(), icon(), vue(),
+		sitemap(), icon(), vue(),
 		import.meta.env.MODE === "production" && compress({
 			img: false
 		})
 	],
+
 	image: {
 		remotePatterns: [{ protocol: "https" }],
-		service: {
-			entrypoint: 'astro/assets/services/sharp'
-		}
 	},
-	compressHTML: false
-});
+	vite: {
+		plugins: [
+			tailwindcss()
+		],
+	},
+	compressHTML: false,
+})
