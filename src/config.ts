a7d7bc359edd5b55ec379da134bfbfdc895f6e08
Hans5958 <hans5958@outlook.com>
2025-05-08 23:13:09 +0700

    Add fediverse:creator meta tag

--- src/config.ts
+++ src/config.ts
@@ -7,6 +7,8 @@ export const SITE_DESCRIPTION = 'The website of Hans5958.'
 export const SITE_AUTHOR = 'Hans5958'
 export const SITE_REPOSITORY = 'https://github.com/Hans5958/hans5958.github.io'
 export const SITE_TWITTER = 'Hans5958'
+export const SITE_MASTODON = '@Hans5958@mastodon.social'
+
 export const COPYRIGHT_YEAR = today.getFullYear()
 export const BASE_URL = import.meta.env.BASE_URL.replace(/\/$/, '')
 
