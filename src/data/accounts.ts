d5803eb3eb779997359b29a4ddecf37bc262948f
Hans5958 <hans5958@outlook.com>
2026-09-21 18:28:29 +0700

    Update Accounts page
    - Add dormant option and icon
    - Remove unnecessary quotation marks
    - Remove unnecessary log messages
    - Remove unnecessary alt text (covered by context)
    - Add more accounts
    - Adjust account notes
    - Use permalink to Bluesky
    - Adjust capitalization of Pokémon GO
    - Remove wrong URL to Battle.net

--- src/data/accounts.ts
+++ src/data/accounts.ts
@@ -6,6 +6,7 @@ export type AccountData = {
 	iconStyle?: string
 	iconImg?: string
 	note: string
+	dormant?: boolean
 }
 
 export type AccountSectionData = {
@@ -19,325 +20,353 @@ export type AccountsData = AccountSectionData[]
 
 export const accounts: AccountData = [
 	{
-		"sectionTitle": "Social Media",
-		"accounts": [
+		sectionTitle: "Social Media",
+		accounts: [
 			{
-				"site": "Twitter",
-				"username": "Hans5958",
-				"url": "https://twitter.com/Hans5958",
-				"icon": "logos:twitter"
+				site: "Twitter",
+				username: "Hans5958",
+				url: "https://twitter.com/Hans5958",
+				icon: "logos:twitter"
 			},
 			{
-				"site": "Twitter",
-				"username": "Tweets5958",
-				"url": "https://twitter.com/Tweets5958",
-				"icon": "logos:twitter"
+				site: "Twitter",
+				username: "Tweets5958",
+				url: "https://twitter.com/Tweets5958",
+				icon: "logos:twitter"
 			},
 			{
-				"site": "YouTube",
-				"username": "Hans5958",
-				"url": "https://www.youtube.com/Hans5958",
-				"icon": "simple-icons:youtube",
-				"iconStyle": "color: red"
+				site: "YouTube",
+				username: "Hans5958",
+				url: "https://www.youtube.com/Hans5958",
+				icon: "simple-icons:youtube",
+				iconStyle: "color: red"
 			},
 			{
-				"site": "YouTube",
-				"username": "Channel5958",
-				"url": "https://www.youtube.com/Channel5958",
-				"icon": "simple-icons:youtube",
-				"iconStyle": "color: red"
+				site: "YouTube",
+				username: "Channel5958",
+				url: "https://www.youtube.com/Channel5958",
+				icon: "simple-icons:youtube",
+				iconStyle: "color: red"
 			},
 			{
-				"site": "Reddit",
-				"username": "Hans5958_",
-				"url": "https://reddit.com/u/Hans5958_",
-				"icon": "logos:reddit-icon"
+				site: "Reddit",
+				username: "Hans5958_",
+				url: "https://reddit.com/u/Hans5958_",
+				icon: "logos:reddit-icon"
 			},
 			{
-				"site": "Discord",
-				"username": "hans5958",
-				"url": "https://discord.com/users/279855717203050496",
-				"icon": "logos:discord-icon",
-				"note": "Not recommended for quick inquiries because you have to add me as a friend. I will accept it, but don't expect for it to be quick."
+				site: "Discord",
+				username: "hans5958",
+				url: "https://discord.com/users/279855717203050496",
+				icon: "logos:discord-icon",
+				note: "Not recommended for quick inquiries because you have to add me as a friend. I will accept it, but don't expect for it to be quick."
 			},
 			{
-				"site": "Mastodon",
-				"username": "Hans5958@mastodon.social",
-				"url": "https://mastodon.social/@Hans5958",
-				"icon": "logos:mastodon-icon"
+				site: "Mastodon",
+				username: "Hans5958@mastodon.social",
+				url: "https://mastodon.social/@Hans5958",
+				icon: "logos:mastodon-icon"
 			},
 			{
-				"site": "Mastodon",
-				"username": "Toots5958@mastodon.social",
-				"url": "https://mastodon.social/@Toots5958",
-				"icon": "logos:mastodon-icon"
+				site: "Mastodon",
+				username: "Toots5958@mastodon.social",
+				url: "https://mastodon.social/@Toots5958",
+				icon: "logos:mastodon-icon"
 			},
 			{
-				"site": "Lemmy",
-				"username": "Hans5958@lemmy.world",
-				"url": "https://lemmy.world/u/Hans5958",
-				"icon": "simple-icons:lemmy",
-				"note": "Rarely used."
+				site: "Lemmy",
+				username: "Hans5958@lemmy.world",
+				url: "https://lemmy.world/u/Hans5958",
+				icon: "simple-icons:lemmy",
+				dormant: true
 			},
 			{
-				"site": "Matrix",
-				"username": "hans5958:matrix.org",
-				"url": "https://matrix.to/#/@hans5958:matrix.org",
-				"icon": "simple-icons:matrix"
+				site: "Matrix",
+				username: "hans5958:matrix.org",
+				url: "https://matrix.to/#/@hans5958:matrix.org",
+				icon: "simple-icons:matrix"
 			},
 			{
-				"site": "Instagram",
-				"username": "hans5958",
-				"url": "https://www.instagram.com/hans5958",
-				"icon": "simple-icons:instagram"
+				site: "Instagram",
+				username: "hans5958",
+				url: "https://www.instagram.com/hans5958",
+				icon: "simple-icons:instagram",
+				dormant: true
 			},
 			{
-				"site": "Bluesky",
-				"username": "hans5958.bsky.social",
-				"url": "https://bsky.app/profile/hans5958.bsky.social",
-				"icon": "simple-icons:bluesky",
-				"iconStyle": "color: #006aff",
-				"note": "Rarely used in favor of Mastodon. Mastodon account is bridged on Hans5958.mastodon.social.ap.brid.gy."
+				site: "Bluesky",
+				username: "hans5958.id",
+				url: "https://bsky.app/profile/did:plc:bwowy3hw2uitqb4c2qt4tod3",
+				icon: "simple-icons:bluesky",
+				iconStyle: "color: #006aff",
+				note: "Rarely used in favor of Mastodon. Mastodon account is bridged on @masto.hans5958.id.",
+				dormant: true
 			},
 			{
-				"site": "Telegram",
-				"username": "Hans5958",
-				"url": "https://t.me/Hans5958",
-				"icon": "logos:telegram"
+				site: "Telegram",
+				username: "Hans5958",
+				url: "https://t.me/Hans5958",
+				icon: "logos:telegram"
 			},
 			{
-				"site": "Twitch",
-				"username": "Hans5958_",
-				"url": "https://twitch.tv/Hans5958_",
-				"icon": "logos:twitch"
+				site: "Twitch",
+				username: "Hans5958_",
+				url: "https://twitch.tv/Hans5958_",
+				icon: "logos:twitch",
+				dormant: true
 			},
 		]
 	},
 	{
-		"sectionTitle": "Software Development",
-		"accounts": [
+		sectionTitle: "Software Development",
+		accounts: [
 			{
-				"site": "GitHub",
-				"username": "Hans5958",
-				"url": "https://github.com/Hans5958",
-				"icon": "simple-icons:github"
+				site: "GitHub",
+				username: "Hans5958",
+				url: "https://github.com/Hans5958",
+				icon: "simple-icons:github"
 			},
 			{
-				"site": "GitHub",
-				"username": "Auto5958",
-				"url": "https://github.com/Auto5958",
-				"icon": "simple-icons:github"
+				site: "GitHub",
+				username: "Auto5958",
+				url: "https://github.com/Auto5958",
+				icon: "simple-icons:github"
 			},
 			{
-				"site": "GitLab",
-				"username": "Hans5958",
-				"url": "https://gitlab.com/Hans5958",
-				"icon": "logos:gitlab-icon"
+				site: "GitLab",
+				username: "Hans5958",
+				url: "https://gitlab.com/Hans5958",
+				icon: "logos:gitlab-icon"
 			},
 			{
-				"site": "GitLab",
-				"username": "Auto5958",
-				"url": "https://gitlab.com/Auto5958",
-				"icon": "logos:gitlab-icon"
+				site: "GitLab",
+				username: "Auto5958",
+				url: "https://gitlab.com/Auto5958",
+				icon: "logos:gitlab-icon"
 			},
 			{
-				"site": "Scratch",
-				"username": "Hans5958",
-				"url": "https://scratch.mit.edu/users/Hans5958",
-				"icon": "simple-icons:scratch",
-				"iconStyle": "color: #FF7B26"
+				site: "Scratch",
+				username: "Hans5958",
+				url: "https://scratch.mit.edu/users/Hans5958",
+				icon: "simple-icons:scratch",
+				iconStyle: "color: #FF7B26",
+				dormant: true
 			},
 			{
-				"site": "Scratch",
-				"username": "Scratcher5958",
-				"url": "https://scratch.mit.edu/users/Scratcher5958",
-				"icon": "simple-icons:scratch",
-				"iconStyle": "color: #FF7B26"
+				site: "Scratch",
+				username: "Scratcher5958",
+				url: "https://scratch.mit.edu/users/Scratcher5958",
+				icon: "simple-icons:scratch",
+				iconStyle: "color: #FF7B26",
+				dormant: true
 			}
 		]
 	},
 	{
-		"sectionTitle": "Gaming",
-		"accounts": [
+		sectionTitle: "Gaming",
+		accounts: [
 			{
-				"site": "Steam",
-				"username": "Hans5958",
-				"url": "https://steamcommunity.com/id/Hans5958",
-				"icon": "simple-icons:steam"
+				site: "Steam",
+				username: "Hans5958",
+				url: "https://steamcommunity.com/id/Hans5958",
+				icon: "simple-icons:steam"
 			},
 			{
-				"site": "Epic Games",
-				"username": "Hans5958",
-				"url": "",
-				"icon": "simple-icons:epicgames"
+				site: "Epic Games",
+				username: "Hans5958",
+				icon: "simple-icons:epicgames"
 			},
 			{
-				"site": "Xbox",
-				"username": "Hans5958",
-				"url": "https://account.xbox.com/en-US/Profile?gamerTag=Hans5958",
-				"icon": "simple-icons:xbox",
-				"iconStyle": "color: #107C10"
+				site: "Xbox",
+				username: "Hans5958",
+				url: "https://account.xbox.com/en-US/Profile?gamerTag=Hans5958",
+				icon: "simple-icons:xbox",
+				iconStyle: "color: #107C10"
 			},
 			{
-				"site": "EA",
-				"username": "Hans5958",
-				"url": "",
-				"icon": "simple-icons:ea"
+				site: "EA",
+				username: "Hans5958",
+				icon: "simple-icons:ea",
+				dormant: true
 			},
 			{
-				"site": "Battle.net",
-				"username": "Hans5958#3236",
-				"url": "",
-				"icon": "simple-icons:battledotnet",
-				"iconStyle": "color: #148EFF"
+				site: "Battle.net",
+				username: "Hans5958#3236",
+				icon: "simple-icons:battledotnet",
+				iconStyle: "color: #148EFF",
+				dormant: true
 			},
 			{
-				"site": "Minecraft",
-				"username": "Hans5958",
-				"url": "https://namemc.com/profile/Hans5958.1",
-				"iconImg": "/assets/img/icons/minecraft.png"
+				site: "Minecraft",
+				username: "Hans5958",
+				url: "https://namemc.com/profile/Hans5958.1",
+				iconImg: "/assets/img/icons/minecraft.png"
 			},
 			{
-				"site": "osu!",
-				"username": "Hans5958",
-				"url": "https://osu.ppy.sh/u/Hans5958",
-				"icon": "simple-icons:osu"
+				site: "osu!",
+				username: "Hans5958",
+				url: "https://osu.ppy.sh/u/Hans5958",
+				icon: "simple-icons:osu"
 			},
 			{
-				"site": "Roblox",
-				"username": "Pixel5958",
-				"url": "https://www.roblox.com/users/3581240125/profile",
-				"icon": "simple-icons:roblox"
+				site: "Roblox",
+				username: "Pixel5958",
+				url: "https://www.roblox.com/users/3581240125/profile",
+				icon: "simple-icons:roblox"
 			},
 			{
-				"site": "TypeRacer",
-				"username": "Hans5958",
-				"url": "https://data.typeracer.com/pit/profile?user=hans5958",
-				"iconImg": "/assets/img/icons/typeracer.svg"
+				site: "TypeRacer",
+				username: "Hans5958",
+				url: "https://data.typeracer.com/pit/profile?user=hans5958",
+				iconImg: "/assets/img/icons/typeracer.svg"
 			},
 			{
-				"site": "Monkeytype",
-				"username": "Hans5958",
-				"url": "https://monkeytype.com/profile/Hans5958",
-				"icon": "simple-icons:monkeytype",
+				site: "Monkeytype",
+				username: "Hans5958",
+				url: "https://monkeytype.com/profile/Hans5958",
+				icon: "simple-icons:monkeytype",
+				dormant: true
 			},
 			{
-				"site": "Speedrun.com",
-				"username": "Hans5958",
-				"url": "https://www.speedrun.com/users/Hans5958",
-				"iconImg": "/assets/img/icons/speedrun-com.svg"
+				site: "Speedrun.com",
+				username: "Hans5958",
+				url: "https://www.speedrun.com/users/Hans5958",
+				iconImg: "/assets/img/icons/speedrun-com.svg",
+				dormant: true
 			},
 			{
-				"site": "Pokémon Go",
-				"username": "Hans5958",
-				"url": "https://monkeytype.com/profile/Hans5958",
-				"icon": "mdi:pokemon-go",
-				"iconStyle": "color: #FF0000; transform: scale(1.2);",
-				"note": "Contact me through other channels if you want to friend me."
+				site: "Pokémon GO",
+				username: "Hans5958",
+				icon: "mdi:pokemon-go",
+				iconStyle: "color: #FF0000; transform: scale(1.2);",
+				note: "Contact me through other channels if you want to friend me."
 			}
 		]
 	},
 	{
-		"sectionTitle": "Collaboration/Volunteering",
-		"accounts": [
+		sectionTitle: "Collaboration/Volunteering",
+		accounts: [
 			{
-				"site": "Wikipedia",
-				"username": "Hans5958",
-				"url": "https://meta.wikimedia.org/wiki/User:Hans5958",
-				"icon": "simple-icons:wikipedia",
-				"note": "Includes other Wikimedia projects such as Wikipedia, Wikidata, and Wikimedia Commons. Common edited wikis are English Wikipedia, Indonesian Wikipedia, and Wikidata."
+				site: "Wikipedia",
+				username: "Hans5958",
+				url: "https://meta.wikimedia.org/wiki/User:Hans5958",
+				icon: "simple-icons:wikipedia",
+				note: "Includes other Wikimedia projects such as Wikipedia, Wikidata, and Wikimedia Commons. Common edited wikis are English Wikipedia, Indonesian Wikipedia, and Wikidata."
 			},
 			{
-				"site": "Wikipedia",
-				"username": "Bot5958",
-				"url": "https://meta.wikimedia.org/wiki/User:Bot5958",
-				"icon": "simple-icons:wikipedia",
-				"note": "For sanctioned, automated edits. See profile to see covered wikis."
+				site: "Wikipedia",
+				username: "Bot5958",
+				url: "https://meta.wikimedia.org/wiki/User:Bot5958",
+				icon: "simple-icons:wikipedia",
+				note: "For sanctioned, automated edits. See profile to see covered wikis."
 			},
 			{
-				"site": "Fandom",
-				"username": "Hans5958",
-				"url": "https://community.fandom.com/wiki/User:Hans5958",
-				"icon": "simple-icons:fandom",
-				"iconStyle": "color: #FA005A",
-				"note": "Covering all Fandom wikis."
+				site: "Fandom",
+				username: "Hans5958",
+				url: "https://community.fandom.com/wiki/User:Hans5958",
+				icon: "simple-icons:fandom",
+				iconStyle: "color: #FA005A",
+				note: "Covering all Fandom wikis."
 			},
 			{
-				"site": "Fandom",
-				"username": "Bot5958",
-				"url": "https://community.fandom.com/wiki/User:Bot5958",
-				"icon": "simple-icons:fandom",
-				"iconStyle": "color: #FA005A",
-				"note": "For sanctioned, automated edits."
+				site: "Fandom",
+				username: "Bot5958",
+				url: "https://community.fandom.com/wiki/User:Bot5958",
+				icon: "simple-icons:fandom",
+				iconStyle: "color: #FA005A",
+				note: "For sanctioned, automated edits."
 			},
 			{
-				"site": "ArchiveTeam",
-				"username": "Hans5958",
-				"url": "https://wiki.archiveteam.org/index.php/User:Hans5958",
-				"iconImg": "https://wiki.archiveteam.org/images/7/76/Archiveteam1.png",
-				"note": "Links to wiki user page. Same name used on the tracker and leaderboards."
+				site: "ArchiveTeam",
+				username: "Hans5958",
+				url: "https://wiki.archiveteam.org/index.php/User:Hans5958",
+				iconImg: "https://wiki.archiveteam.org/images/7/76/Archiveteam1.png",
+				note: "Links to wiki user page. Same name used on the tracker and leaderboards."
 			},
 			{
-				"site": "VocaDB",
-				"username": "Hans5958",
-				"url": "https://vocadb.net/Profile/Hans5958",
-				"iconImg": "https://static.vocadb.net/img/favicon.ico",
+				site: "VocaDB",
+				username: "Hans5958",
+				url: "https://vocadb.net/Profile/Hans5958",
+				iconImg: "https://static.vocadb.net/img/favicon.ico",
 			},
 			{
-				"site": "UtaiteDB",
-				"username": "Hans5958",
-				"url": "https://utaitedb.net/Profile/Hans5958",
-				"iconImg": "https://static.utaitedb.net/img/favicon.ico",
+				site: "UtaiteDB",
+				username: "Hans5958",
+				url: "https://utaitedb.net/Profile/Hans5958",
+				iconImg: "https://static.utaitedb.net/img/favicon.ico",
 			},
 			{
-				"site": "TouhouDB",
-				"username": "Hans5958",
-				"url": "https://touhoudb.com/Profile/Hans5958",
-				"iconImg": "https://static.touhoudb.com/img/favicon.ico",
+				site: "TouhouDB",
+				username: "Hans5958",
+				url: "https://touhoudb.com/Profile/Hans5958",
+				iconImg: "https://static.touhoudb.com/img/favicon.ico",
 			},
 			{
-				"site": "Minecraft Wiki",
-				"username": "Hans5958",
-				"url": "https://minecraft.wiki/wiki/User:Hans5958",
-				"iconImg": "https://minecraft.wiki/images/Wiki_textless.png?format=original",
-				"iconStyle": "transform: scale(1.3);",
+				site: "Minecraft Wiki",
+				username: "Hans5958",
+				url: "https://minecraft.wiki/wiki/User:Hans5958",
+				iconImg: "https://minecraft.wiki/images/Wiki_textless.png?format=original",
+				iconStyle: "transform: scale(1.3);",
+				dormant: true
 			},
 			{
-				"site": "SiIvaGunner Wiki",
-				"username": "Hans5958",
-				"url": "https://siivagunner.wiki/wiki/User:Hans5958",
-				"iconImg": "/assets/img/icons/siivagunner-wiki.svg"
+				site: "SiIvaGunner Wiki",
+				username: "Hans5958",
+				url: "https://siivagunner.wiki/wiki/User:Hans5958",
+				iconImg: "/assets/img/icons/siivagunner-wiki.svg"
 			},
 		]
 	},
 	{
-		"sectionTitle": "Other",
-		"accounts": [
+		sectionTitle: "Other",
+		accounts: [
 			{
-				"site": "Keybase",
-				"username": "hans5958",
-				"url": "https://keybase.io/hans5958",
-				"iconImg": "/assets/img/icons/keybase.svg"
+				site: "Keybase",
+				username: "hans5958",
+				url: "https://keybase.io/hans5958",
+				iconImg: "/assets/img/icons/keybase.svg"
 			},
 			{
-				"site": "dev.to",
-				"username": "hans5958",
-				"url": "https://dev.to/hans5958",
-				"icon": "simple-icons:devdotto"
+				site: "dev.to",
+				username: "hans5958",
+				url: "https://dev.to/hans5958",
+				icon: "simple-icons:devdotto",
+				dormant: true
 			},
 			{
-				"site": "Last.fm",
-				"username": "Hans5958",
-				"url": "https://www.last.fm/user/Hans5958",
-				"icon": "simple-icons:lastdotfm",
-				"iconStyle": "color: #D51007"
+				site: "Last.fm",
+				username: "Hans5958",
+				url: "https://www.last.fm/user/Hans5958",
+				icon: "simple-icons:lastdotfm",
+				iconStyle: "color: #D51007"
 			},
 			{
-				"site": "Trakt",
-				"username": "Hans5958",
-				"url": "https://trakt.tv/users/hans5958",
-				"icon": "simple-icons:trakt",
-				"iconStyle": "color: #ED1C24"
+				site: "Trakt",
+				username: "Hans5958",
+				url: "https://trakt.tv/users/hans5958",
+				icon: "simple-icons:trakt",
+				iconStyle: "color: #ED1C24"
+			},
+			{
+				site: "Pastebin",
+				username: "Hans5958",
+				url: "https://pastebin.com/u/Hans5958",
+				icon: "simple-icons:pastebin",
+				iconStyle: "color: #02456C"
+			},
+			{
+				site: "Straw.Page",
+				username: "Hans5958",
+				url: "https://hans5958.straw.page",
+				iconImg: "https://straw.page/apple-touch-icon.png?v=2",
+			},
+			{
+				site: "DeviantArt",
+				username: "Hans5958",
+				url: "https://deviantart.com/Hans5958",
+				icon: "simple-icons:deviantart",
+				iconStyle: "color: #05CC47",
+				dormant: true
 			}
 		]
 	}
